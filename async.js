308756d87d91cb11feed3bda42105f4f539abb32
Unknown <Danil.Koporushkin@urfu.me>
2017-11-30 13:49:36 +0500

    еще

--- async.js
+++ async.js
@@ -1,4 +1,6 @@
 'use strict';
+/* eslint-disable */
+
 
 exports.isStar = true;
 exports.runParallel = runParallel;
@@ -10,4 +12,51 @@ exports.runParallel = runParallel;
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
     // асинхронная магия
+    let queueOfJobs;
+    queueOfJobs = jobs
+        .map(_timer(timeout))
+        .map((__element, __index) => [__element, __index]);
+    let __res = [];
+    let __countFinished = 0;
+
+    function allRes(resolve, jobRes, jobIndex) {
+        __res[jobIndex] = jobRes;
+        ++__countFinished;
+        if (jobs.length === __countFinished) {
+            resolve(__res);
+        } else if (queueOfJobs.length) {
+            resolveJobs(resolve, ... queueOfJobs.shift());
+        }
+    }
+
+    function resolveJobs(resolve, job, i) {
+        let handler = jobResult => allRes(resolve, jobResult, i);
+        job()
+            .catch(handler)
+            .then(handler)
+            .catch(handler);
+    }
+
+    return new Promise(resolve => {
+        let queue;
+        if (parallelNum > 0 && jobs.length) {
+            queue = queueOfJobs.splice(parallelNum);
+            queueOfJobs.splice(parallelNum);
+            queue
+                .forEach(([__element, __index]) => resolveJobs(resolve, __element, __index));
+        } else {
+            resolve(__res);
+        }
+    });
+}
+
+
+function _timer(timeout) {
+    return jobs => () => new Promise((resolve, reject) =>{
+        jobs()
+            .catch(reject)
+            .then(resolve)
+            .catch(reject);
+        setTimeout(() => reject(new Error('run out of time')), timeout);
+    });
 }
